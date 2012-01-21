b6ecc42c56739045ca6ed3ba73076bdf55dd3b1a
Graeme Foster <graemef@gmail.com>
2012-01-21 20:39:44 +0000

    Use jQuery to fade stuff.

---
+++ KnockoutDemo/scripts/libs/knockout-fadeVisible.js
@@ -0,0 +1,15 @@
+define(["knockout", "jquery"], function (ko, $) {
+    // Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
+    ko.bindingHandlers.fadeVisible = {
+        init:function (element, valueAccessor) {
+            // Initially set the element to be instantly visible/hidden depending on the value
+            var value = valueAccessor();
+            $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+        },
+        update:function (element, valueAccessor) {
+            // Whenever the value subsequently changes, slowly fade the element in or out
+            var value = valueAccessor();
+            ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+        }
+    };
+});
--- KnockoutDemo/scripts/main.js
+++ KnockoutDemo/scripts/main.js
@@ -3,11 +3,12 @@ require.config({
         jquery:'libs/require-jquery',
         knockout:'libs/require-knockout',
         "knockout-onDemand":'libs/knockout-onDemand',
+        "knockout-fadeVisible":'libs/knockout-fadeVisible',
         underscore:'libs/underscore'
     }
 });
 
-require(["jquery", "knockout", "root", "service"], function ($, ko, Root, Service) {
+require(["jquery", "knockout", "root", "service", "knockout-fadeVisible"], function ($, ko, Root, Service) {
     $(function () {
         ko.applyBindings(
             new Root(new Service("http://localhost:1337/")));
