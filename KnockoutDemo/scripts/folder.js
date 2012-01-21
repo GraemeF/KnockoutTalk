823915ae5fc2089d866a84a0313c526066c8b52e
Graeme Foster <graemef@gmail.com>
2012-01-21 18:28:12 +0000

    Fixed breadcrumbs.

--- KnockoutDemo/scripts/folder.js
+++ KnockoutDemo/scripts/folder.js
@@ -1,29 +1,42 @@
-define(["knockout", "knockout-onDemand"], function(ko) {
-    var Folder = function(service, activeFolder, dto) {
+define(["knockout", "knockout-onDemand"], function (ko) {
+    var Folder = function (service, activeFolder, parent, dto) {
         this.service = service;
         this.name = dto.name;
         this.path = dto.path;
         this.activeFolder = activeFolder;
+        this.parent = parent;
         this.contents = ko.onDemandObservableArray(this.getContents, this);
         this.isExpanded = ko.observable(false);
+        this.isActive = ko.computed(function () {
+            return this.activeFolder() === this;
+        }, this);
     };
 
-    Folder.prototype.activate = function() {
+    Folder.prototype.activate = function () {
         this.activeFolder(this);
+        this.ensureExpanded();
     };
 
-    Folder.prototype.toggleExpanded = function() {
+    Folder.prototype.toggleExpanded = function () {
         this.isExpanded(!this.isExpanded());
         this.activate();
     };
 
-    Folder.prototype.getContents = function() {
+    Folder.prototype.ensureExpanded = function () {
+        this.isExpanded(true);
+        if (this.parent !== null)
+            this.parent.ensureExpanded();
+    };
+
+    Folder.prototype.getContents = function () {
         var self = this;
-        this.service.getContents(self.path, function(error, data) {
+        this.service.getContents(self.path, function (error, data) {
             if (error !== null)
                 throw error;
 
-            self.contents(_.map(data, function(dto) { return new Folder(self.service, self.activeFolder, dto); }, self));
+            self.contents(_.map(data, function (dto) {
+                return new Folder(self.service, self.activeFolder, self, dto);
+            }, self));
         });
     };
 
--- KnockoutDemo/scripts/root.js
+++ KnockoutDemo/scripts/root.js
@@ -2,12 +2,14 @@ define(["knockout", "folder"], function (ko, Folder) {
     var Root = function (service) {
         this.service = service;
         this.activeFolder = ko.observable(null);
-        this.rootFolder = new Folder(service, this.activeFolder, {"name":"Home", "path":"", "uri":"/"});
+        this.rootFolder = new Folder(service, this.activeFolder, null, {"name":"Home", "path":"", "uri":"/"});
         this.activeFolder(this.rootFolder);
 
         this.activate = function (folder) {
             this.activeFolder(folder);
-        }
+        };
+
+        this.rootFolder.activate();
     };
 
     return Root;
